ec54efd994b8fd1d933cbf955ac7ac73b480b7a2
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-09-26 14:23:31 +0700

    Hello World

---
+++ src/pertemuan_05/README.md
@@ -0,0 +1,63 @@
+# Pemrograman Mobile - Pertemuan 5
+
+<table>
+    <thead>
+        <th style="text-align: center;" colspan="2">Pertemuan 5</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Nama</td>
+            <td>Ridho Aulia' Rahman</td>
+        </tr>
+        <tr>
+            <td>Nim</td>
+            <td>2241720162</td>
+        </tr>
+    </tbody>
+</table>
+
+# Praktikum 1: Membuat Project Flutter Baru
+
+**Langkah 1:**
+
+Membuat project baru dengan nama `hello_world`.
+
+Hasil:
+
+![Hasil Create Project](../../docs/pertemuan_05_1.png)
+
+# Praktikum 2: Menghubungkan Perangkat Android atau Emulator
+
+![Hasil Running di device](../../docs/pertemuan_05_2.png)
+
+# Praktikum 3: Membuat Repository GitHub dan Laporan Praktikum
+
+![Repository Mobile](../../docs/pertemuan_05_3.png)
+
+# Praktikum 4: Menerapkan Widget Dasar
+
+**Langkah 1: Text Widget**
+
+Buat folder baru basic_widgets di dalam folder lib. Kemudian buat file baru di dalam basic_widgets dengan nama text_widget.dart. Ketik atau salin kode program berikut ke project hello_world Anda pada file text_widget.dart.
+
+![Text Widget](../../docs/pertemuan_05_4.png)
+
+Lakukan import file text_widget.dart ke main.dart, lalu ganti bagian text widget dengan kode di atas. Maka hasilnya seperti gambar berikut. Screenshot hasil milik Anda, lalu dibuat laporan pada file README.md.
+
+![Hasil Penambahan Text Widget](../../docs/pertemuan_05_5.png)
+
+**Langkah 2: Image Widget**
+
+Buat sebuah file image_widget.dart di dalam folder basic_widgets dengan isi kode berikut.
+
+![image widget](../../docs/pertemuan_05_6.png)
+
+# Praktikum 5: Menerapkan Widget Material Design dan iOS Cupertino
+
+**Langkah 1: Cupertino Button dan Loading Bar**
+
+![Cupertino](../../docs/pertemuan_05_7.png)
+
+**Langkah 2: Floating Action Button (FAB)**
+
+![alt](https://)
---
+++ src/pertemuan_05/hello_world/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/basic_widget/image_widget.dart';
+import 'package:hello_world/basic_widget/loading_cuppertino.dart';
+import 'package:hello_world/basic_widget/text_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const MyCustomCuppertino(),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
